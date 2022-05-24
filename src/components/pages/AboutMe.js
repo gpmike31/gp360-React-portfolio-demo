f50b5c7a6a2b970945823bfd28e0b617dfd42acd
gpmike31 <gpmike3105@gmail.com>
2022-05-23 21:21:16 -0500

    fixed img tag for about me section

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -3,7 +3,7 @@ export default function AboutMe(){
     return (
         <div className="h-50 d-flex mx-5 my-5 justify-content-start">
             <figure className="figure d-flex flex-column align-items-start w-50">
-                <img src="./graphics/me-programming.png" alt="Me programming" className="figure-img image-fluid w-50 rounded d-flex"></img>
+                <img src="./graphics/profile-pic.jpg" alt="profile pic" className="figure-img image-fluid w-50 rounded d-flex"></img>
                 <figcaption className="figure-caption text text-color d-flex">Me, programming</figcaption>
             </figure>
             <div className="text text-color">
